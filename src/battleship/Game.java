90cc2cf4ebada57d590e99048482046e761bde6d
Cyrus Xi <xi.cyrus@gmail.com>
2014-10-15 19:39:00 -0400

    Fix indentation issues

--- src/battleship/Game.java
+++ src/battleship/Game.java
@@ -7,13 +7,13 @@
  *      09/20/14.
  * 
  * To build and run from command line:
- * 	    Be in project root directory (above src and bin).
- * 	    To compile:
+ *      Be in project root directory (above src and bin).
+ *      To compile:
  * 		    Type: javac -Xlint -d bin src/battleship/*.java
  * 		    ("battleship" is the package name. To maintain the bin & src
  * 		    distinction, this compiles all the .java files in src/battleship and
  * 		    then puts the .class files into the bin directory.)
- * 	    To run:
+ *      To run:
  * 	        Uncomment this line -- fname = "src/" + fname; -- in GameBoard class.
  * 	        Type: java -cp bin battleship.Game
  * 		    (This first gets the right ships.txt file, then temporarily sets
